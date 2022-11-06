56e4f555361ac0620bb96668873c9dd2c40af558
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-06 14:45:36 -0300

    chore: add function fizzbuzz

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,7 +1,9 @@
 import { expect, it, describe } from 'vitest';
 
+const fizzbuzz = () => {}
+
 describe('fizzbuzz', () => {
   it('should be a function', () => {
-    expect(typeof (fizzbuzz)).toBe('function')
+    expect(typeof fizzbuzz).toBe('function')
   });
 });
