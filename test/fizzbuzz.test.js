c7486a95229eb926a286f793e198a5fcf35da247
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-06 15:06:25 -0300

    refactor: Fizzbuzz test

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,9 +1,27 @@
 import { expect, it, describe } from 'vitest';
 
-const fizzbuzz = () => {}
+/*
+Escribir una función que al pasarle un número
+  - Muestra "fizz" si es multiplo de 3
+  - Muestra "buzz" si es multiplo de 5
+  - Muestra "fizzbuzz" si es multiplo de 3 y 5
+  - Muestra el número si no es multiplo de de los anteriores
+*/
+
+const fizzbuzz = (number) => {
+    if (typeof number != 'number') throw new Error('parameter provided must be a number')
+}
 
 describe('fizzbuzz', () => {
   it('should be a function', () => {
     expect(typeof fizzbuzz).toBe('function')
+  });
+
+  it('should throw if not number is provided as parameters', () => {
+    expect(() => fizzbuzz()).toThrow()
+  });
+
+  it('should throw a specific error message if not number is provided as parameters', () => {
+    expect(() => fizzbuzz()).toThrow(/number/)
   });
 });
