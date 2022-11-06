273db67d3ef47cea29cb4287260ed60b24f7a636
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-06 13:33:40 -0300

    chore: add module config and first test

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'vitest';
+import { it } from 'vitest';
+import { describe } from 'vitest'
+
+describe('fizzbuzz', () => {
+  it('should be a function', () => {
+    expect(typeof (fizzbuzz)).toBe('function')
+  });
+});
