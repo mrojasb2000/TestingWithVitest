e2bac6b51022b6bfacc26eb3d6a989edb7657f30
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-07 20:00:38 -0300

    chore: add number provided as parameters

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -11,6 +11,7 @@ Escribir una función que al pasarle un número
 const fizzbuzz = (number) => {
     if (typeof number != 'number') throw new Error('parameter provided must be a number')
     if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
+    return number;
 }
 
 describe('fizzbuzz', () => {
@@ -28,5 +29,13 @@ describe('fizzbuzz', () => {
 
   it('should throw a specific error message not number is provided', () => {
     expect(() => fizzbuzz(NaN)).toThrow(/number/)
+  });
+
+  it('should return 1 if number provided is 1', () => {
+    expect(fizzbuzz(1)).toBe(1)
+  });
+
+  it('should return 2 if number provided is 2', () => {
+    expect(fizzbuzz(2)).toBe(2)
   });
 });
