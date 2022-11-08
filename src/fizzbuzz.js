df6415696d7ace9f5fcc31769755cb7f9c8b1631
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-07 22:35:02 -0300

    refactor: implementation of fizzbuzz function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -9,10 +9,16 @@ Escribir una función que al pasarle un número
 export const fizzbuzz = (number) => {
     if (typeof number != 'number') throw new Error('parameter provided must be a number')
     if (Number.isNaN(number)) throw new Error('parameter provided must be a number')
-
-    if (number % 15 == 0 ) return 'fizzbuzz'
-    if (number % 3 == 0) return 'fizz'
-    if (number % 5 == 0) return 'buzz'
     
-    return number;
+    const multiplies = { 3: 'fizz', 5: 'buzz' }
+
+    let output = ''
+
+    Object
+        .entries(multiplies)
+        .forEach(([multiplies, word]) => {
+            if (number % multiplies == 0) output += word
+        });
+    
+    return output === '' ? number :  output;
 }
